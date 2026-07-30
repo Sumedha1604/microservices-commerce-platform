9a16cc0dbf95854e3dbba4d090f49a8200ad70d5
Sumedha Thakur <sumedhathakur123@gmail.com>
2026-07-30 06:09:30 +0200

    feat: add shared API constants

---
+++ shared/common-core/src/main/java/com/sumedha/commerce/common/core/constants/ApiConstants.java
@@ -0,0 +1,14 @@
+package com.sumedha.commerce.common.core.constants;
+
+public final class ApiConstants {
+
+    public static final String DEFAULT_SUCCESS_MESSAGE = "Success";
+    public static final String DEFAULT_ERROR_MESSAGE = "An unexpected error occurred";
+    public static final int DEFAULT_PAGE_NUMBER = 0;
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final int MAX_PAGE_SIZE = 100;
+
+    private ApiConstants() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+}
